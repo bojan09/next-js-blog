8d789112f9acd03223013e561a2717f9702ba4e7
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-24 10:32:31 +0200

    minor updates in initial setup

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      {/* Text */}
+      <div>
+        ©2023 SimpleBlog <br /> All rights reserved.
+      </div>
+
+      {/* Social Images */}
+      <div>Social Images</div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,27 @@
+"use client";
 import React from "react";
 
+import Link from "next/link";
+
+// links
+import links from "@/constants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link href={"/"}>SimpleBlog</Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+
+        {/* Logout button */}
+        <button onClick={() => console.log("Logged Out")}>Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
