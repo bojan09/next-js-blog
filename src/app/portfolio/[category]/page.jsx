957b60ae6bba1dd62dd6373d812b2b733f967d68
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-07-01 09:44:59 +0200

    adding Portfolio - category

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+// components
+import { Button } from "@/components";
+
+const Category = ({ params }) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-[#53c26b] capitalize">
+        {params.category}
+      </h1>
+
+      {/*Category Item*/}
+      <div className="category_item">
+        {/*Category Item - Content*/}
+        <div className="flex-1 flex flex-col gap-6 justify-center items-center category_item">
+          <h1 className="text-5xl">Test</h1>
+          <p className="text-xl">Desc</p>
+          <Button text="See more" url="#" />
+        </div>
+
+        {/*Category Item - Image container*/}
+        <div className="relative">
+          <Image
+            src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="test_img"
+            width={500}
+            height={600}
+            className="object-contain rounded-md"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
