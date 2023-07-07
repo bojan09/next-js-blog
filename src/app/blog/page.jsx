919b5e27482f0ea0ad733d2d8b21819ce400c6e6
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-07-07 20:04:44 +0200

    Blog Mobile Update

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -9,9 +9,9 @@ const Blog = () => {
   return (
     <div className="flex flex-col justify-evenly">
       {/* Blog */}
-      <div className="flex justify-evenly my-[5rem]">
+      <div className="md:flex justify-evenly my-[5rem]">
         {/*Blog Item - Image container*/}
-        <div className="mr-12 my-auto">
+        <div className="md:mr-12 my-auto xs:mx-auto">
           <Image
             src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="test_img"
@@ -23,10 +23,10 @@ const Blog = () => {
         {/*Blog Item - Content container*/}
         <Link
           href="/blog/testId"
-          className="flex flex-col gap-6 justify-center"
+          className="flex flex-col gap-6 justify-center md:my-0 xs:my-7"
         >
           <h1 className="text-5xl">Test</h1>
-          <p className="text-lg  w-[90ch]">
+          <p className="text-lg  md:w-[90ch] xs:w-[30ch] xs:mb-4 md:m-0">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
             distinctio voluptatum laudantium nihil accusantium rem quod ut
             dignissimos molestias autem fugiat labore dolor natus commodi, illo
