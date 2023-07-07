bfdf3ff8d050a796040eb0822dc165edec8d2707
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-07-07 19:57:08 +0200

    Mobile updates in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
       </div>
 
       {/* Mobile Navbar */}
-      <div className="md:hidden xs:relative text-[#222]">
+      <div className="md:hidden xs:relative text-[#222] z-10">
         <Image
           src={!toggle ? menu : close}
           className="fixed top-6 right-7 object-contain cursor-pointer"
@@ -51,17 +51,25 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } flex-col justify-center gap-1 mt-[17rem] mr-[-2.6rem] py-2 px-4 p  bg-white`}
+          } flex-col justify-center gap-1 mt-[21rem] mr-[-1.5rem] py-2 px-4 p  bg-gradient-to-tr from-gray-50 to-gray-200 rounded-sm`}
         >
+          <div className="my-2 mx-auto">
+            <DarkModeToggle />
+          </div>
+
           {links.map((link) => (
-            <Link key={link.id} href={link.url}>
+            <Link
+              key={link.id}
+              href={link.url}
+              onClick={() => setActive(link.title) || window.scrollTo(0, 0)}
+            >
               {link.title}
             </Link>
           ))}
 
           {/* Logout button */}
           <button
-            className="px-2 py-1 border-none bg-[#53c28b] hover:bg-[#53c26b] rounded-md text-[#121212] mt-2 mb-1"
+            className=" px-2 py-1 border-none bg-[#53c28b] hover:bg-[#53c26b] rounded-md text-[#121212] mt-3 mb-1"
             onClick={() => console.log("Logged Out")}
           >
             Logout
