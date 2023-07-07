73ad1b1b62f5826538dec8d26c1b1a22635aea82
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-07-07 20:27:46 +0200

    Portfolio page, mobile updates

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -14,14 +14,35 @@ const Category = ({ params }) => {
       {/*Category Item*/}
       <div className="category_item">
         {/*Category Item - Content*/}
-        <div className="flex-1 flex flex-col gap-6 justify-center items-center ">
-          <h1 className="text-5xl">Test</h1>
-          <p className="text-xl">Desc</p>
-          <Button text="See more" url="#" />
+        <div className="md:flex-1 flex flex-col gap-6 justify-center">
+          <h1 className="text-3xl md:text-5xl">Lorem ipsum dolor sit amet.</h1>
+
+          {/*Category Item - Image container*/}
+          <div className="relative xs:inline-block md:hidden">
+            <Image
+              src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="test_img"
+              width={500}
+              height={600}
+              className="object-contain rounded-md"
+            />
+          </div>
+
+          <p className="text-lg md:text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure modi
+            quam ducimus, velit dolor asperiores ipsa quas beatae nisi fugiat
+            iusto, aliquid ipsam dignissimos porro molestias optio et
+            voluptatibus. Impedit corporis nam unde molestias aspernatur tenetur
+            maiores dolorem deleniti, quis, ducimus vitae eum voluptatibus iusto
+            excepturi velit sunt fugiat itaque.
+          </p>
+          <div className="my-4">
+            <Button text="See more" url="#" />
+          </div>
         </div>
 
         {/*Category Item - Image container*/}
-        <div className="relative">
+        <div className="relative xs:hidden md:inline-block">
           <Image
             src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="test_img"
