7ebfa14a17555a2b80b396f411f09043d850d699
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-07-13 06:20:55 +0200

    server side data fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,38 +5,47 @@ import Link from "next/link";
 // components
 import { Button } from "@/components";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: { revalidate: 10 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+
   return (
     <div className="flex flex-col justify-evenly">
-      {/* Blog */}
-      <div className="md:flex justify-evenly my-[5rem]">
-        {/*Blog Item - Image container*/}
-        <div className="md:mr-12 my-auto xs:mx-auto">
-          <Image
-            src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="test_img"
-            width={300}
-            height={300}
-            className="object-contain rounded-md"
-          />
+      {data.map((item) => (
+        <div className="md:flex justify-evenly my-[5rem]" key={item.id}>
+          {/*Blog Item - Image container*/}
+          <div className="md:mr-12 my-auto xs:mx-auto">
+            <Image
+              src="https://images.pexels.com/photos/3021382/pexels-photo-3021382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="test_img"
+              width={600}
+              height={600}
+              className="object-cover rounded-md"
+            />
+          </div>
+          {/*Blog Item - Content container*/}
+          <Link
+            href="/blog/testId"
+            className="flex flex-col gap-6 justify-center md:my-0 xs:my-7"
+          >
+            <h1 className="text-5xl">{item.title}</h1>
+            <p className="text-lg  md:w-[90ch] xs:w-[30ch] xs:mb-4 md:m-0">
+              {item.body}
+            </p>
+            <Button text="See more" url="#" />
+          </Link>
         </div>
-        {/*Blog Item - Content container*/}
-        <Link
-          href="/blog/testId"
-          className="flex flex-col gap-6 justify-center md:my-0 xs:my-7"
-        >
-          <h1 className="text-5xl">Test</h1>
-          <p className="text-lg  md:w-[90ch] xs:w-[30ch] xs:mb-4 md:m-0">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            distinctio voluptatum laudantium nihil accusantium rem quod ut
-            dignissimos molestias autem fugiat labore dolor natus commodi, illo
-            expedita pariatur explicabo. Tempore ipsa itaque deleniti illum
-            magni excepturi voluptatum pariatur, amet aspernatur, laudantium
-            magnam ipsam est modi.
-          </p>
-          <Button text="See more" url="#" />
-        </Link>
-      </div>
+      ))}
     </div>
   );
 };
