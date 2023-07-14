649943f54c36a7dd2c75ad4a93ee92f7b2a06f64
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-07-14 06:37:22 +0200

    adding Mongoose & connecting to DB

---
+++ models/postModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    contet: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("postModel", postSchema);
---
+++ src/utilities/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+  } catch {
+    throw new Error("Connection failed");
+  }
+};
+
+export default connect;
